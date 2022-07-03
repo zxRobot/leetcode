207ca5a14974bb8fa5bc10070bf410501d4568c9
“zhangxu” <928402978@qq.com>
2022-07-03 12:19:40 +0800

    0015

--- src/0015_three_sum.cpp
+++ src/0015_three_sum.cpp
@@ -6,22 +6,35 @@ namespace leetcode::three_sum
     std::vector<std::vector<int>> Solution::threeSum(std::vector<int> &nums)
     {
         std::vector<std::vector<int>> res;
+        if (nums.size() < 3)
+        {
+            return res;
+        }
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 2; i++)
         {
+            if (i != 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
             for (int j = i + 1; j < nums.size() - 1; j++)
             {
-                int target = -nums[i] - nums[j];
-                int k = j + 1;
-                while (target >= nums[k])
+                if (j != 0 && nums[j] == nums[j - 1] && j - i > 1)
                 {
-                    for (int k = j + 1; k < nums.size(); k++)
+                    continue;
+                }
+
+                int target = -nums[i] - nums[j];
+                if (nums[j + 1] > target)
+                {
+                    continue;
+                }
+                for (int k = j + 1; k < nums.size(); k++)
+                {
+                    if (nums[k] == target)
                     {
-                        if (nums[k] == target)
-                        {
-                            res.push_back({nums[i], nums[j], nums[k]});
-                            break;
-                        }
+                        res.push_back({nums[i], nums[j], nums[k]});
+                        break;
                     }
                 }
             }
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     leetcode::three_sum::Solution s;
-    std::vector<int> input = {-1,0,1,2,-1,-4};
+    std::vector<int> input = {0};
     std::vector<std::vector<int>> res = s.threeSum(input);
     //std::cout<<res<<std::endl;
     return 0;

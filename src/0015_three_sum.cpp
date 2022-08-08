dd3b99a67ca00748202f97ecae58004aa941f91f
“zhangxu” <928402978@qq.com>
2022-08-08 23:16:59 +0800

    three_sum

--- src/0015_three_sum.cpp
+++ src/0015_three_sum.cpp
@@ -1,8 +1,29 @@
 #include <algorithm>
+#include <set>
 #include "0015_three_sum.h"
 
 namespace leetcode::three_sum
 {
+    std::vector<std::vector<int>> twoSum(std::vector<int> &nums, int begin, int target)
+    {
+        std::set<int> set;
+        for (int i = begin; i < nums.size(); i++)
+        {
+            set.insert(nums[i]);
+        }
+        std::vector<std::vector<int>> res;
+        for (int i = begin; i < nums.size(); i++)
+        {
+            if (set.find(target - nums[i]) != set.end())
+            {
+                std::vector<int> temp;
+                temp.push_back(nums[i]);
+                temp.push_back(target - nums[i]);
+                res.push_back(temp);
+            }
+        }
+    }
+
     std::vector<std::vector<int>> Solution::threeSum(std::vector<int> &nums)
     {
         std::vector<std::vector<int>> res;
@@ -13,29 +34,22 @@ namespace leetcode::three_sum
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 2; i++)
         {
+            if (nums[i] > 0)
+            {
+                break;
+            }
             if (i != 0 && nums[i] == nums[i - 1])
             {
                 continue;
             }
-            for (int j = i + 1; j < nums.size() - 1; j++)
+            int target = -nums[i];
+            std::vector<std::vector<int>> s = twoSum(nums, i + 1, target);
+            if (!s.empty())
             {
-                if (j != 0 && nums[j] == nums[j - 1] && j - i > 1)
+                for (int k = 0; k < s.size(); k++)
                 {
-                    continue;
-                }
-
-                int target = -nums[i] - nums[j];
-                if (nums[j + 1] > target)
-                {
-                    continue;
-                }
-                for (int k = j + 1; k < nums.size(); k++)
-                {
-                    if (nums[k] == target)
-                    {
-                        res.push_back({nums[i], nums[j], nums[k]});
-                        break;
-                    }
+                    s[k].push_back(nums[i]);
+                    res.push_back(s[k]);
                 }
             }
         }
